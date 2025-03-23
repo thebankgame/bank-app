336fdebc86ee7ee4421bb7e9014b6359a08d6f95
thebankgame <thebankgame.com@gmail.com>
2025-03-23 11:56:51 -0700

    eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,51 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import globals from "globals";
+import reactPlugin from "eslint-plugin-react";
+import typescriptPlugin from "@typescript-eslint/eslint-plugin";
+import importPlugin from "eslint-plugin-import"; // Import the "import" plugin
+import typescriptParser from "@typescript-eslint/parser"; // Import the parser
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+export default [
+  {
+    ignores: ["**/dev", "**/.next/*", "**/temp.js", "config/*"]
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.es2021,
+        ...globals.node,
+      },
+      ecmaVersion: 2021,
+      sourceType: "module",
+      parser: typescriptParser, // Use the imported parser object
+    },
+    plugins: {
+      react: reactPlugin,
+      "@typescript-eslint": typescriptPlugin,
+      import: importPlugin, // Add the "import" plugin
+    },
+    rules: {
+      "react/no-unescaped-entities": "error",
+      "no-restricted-syntax": [
+        "error",
+        {
+          selector:
+            "CallExpression[callee.property.name='push'] > SpreadElement.arguments",
+          message: "Do not use spread arguments in Array.push",
+        },
+      ],
+      "no-unused-vars": "error",
+      "import/no-duplicates": "error", // Ensure this rule works with the "import" plugin
+      "import/no-unresolved": "off",
+      "import/order": "off",
+      "simple-import-sort/imports": "off",
+      "@typescript-eslint/no-explicit-any": "error",
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
 ];
-
-export default eslintConfig;
